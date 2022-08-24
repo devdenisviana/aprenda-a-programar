c5b3098e9912fa38f0c4b245bc3683a7da690a7e
Denis Viana <denisviana@gmail.com>
2022-08-24 13:25:44 -0300

    resolução dos desafios

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -5,3 +5,7 @@ var idade = 43;
 var sonho = "Antuérpia";
 
 console.log("nome "+nome,"email "+email,"profissao "+profissao,"idade "+idade,"sonho "+sonho);
+console.log("Olá "+nome+"!");
+console.log("Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens áres para "+sonho+". Confirma para gente alguns dados?")
+console.log("A sua idade é "+idade+" ,você é "+profissao+" e o seu email é "+email+" ?");
+console.log("Ficamos no aguardo. Muito obrigado e boa viagem :D");
