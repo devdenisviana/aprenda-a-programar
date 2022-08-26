3ab660f710996f60719825d4b12738c57f1bcc6a
Denis Viana <denisviana@gmail.com>
2022-08-26 11:24:17 -0300

    codigo numeros impares

---
+++ tarefa-dia-3/pratica-3.js
@@ -0,0 +1,14 @@
+// exibir todos os numeros impares de uma faixa de valores
+
+let num1 = 1
+let num2 = 10
+let cont = 1
+
+while (num1 <= num2)
+{
+    if (num1 % 2 == 1)
+        {
+            console.log(num1)
+        }    
+    num1++
+}
