0c4d73ee484ce9498d42aaa8a7da4c08e0e00886
Denis Viana <denisviana@gmail.com>
2022-08-26 08:38:28 -0300

    pratica da tabuada

---
+++ tarefa-dia-3/pratica-2.js
@@ -0,0 +1,11 @@
+//tabuada
+let mult = 9
+let n = 10
+let cont = 1
+
+while (cont <= n){
+    let result = mult * cont
+    let textoDeExibicao = `${mult} x ${cont} = ${result}`
+    console.log(textoDeExibicao)
+    cont++
+}
